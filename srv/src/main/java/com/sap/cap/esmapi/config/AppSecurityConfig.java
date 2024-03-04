3092ae27b736bd5da0d340b764b88b2f7545a8cb
Sunny Bhardwaj <sunny13nitk@gmail.com>
2024-03-04 09:08:12 +0530

    initial commit

---
+++ srv/src/main/java/com/sap/cap/esmapi/config/AppSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.sap.cap.esmapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.HeaderWriterLogoutHandler;
+import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter;
+
+import com.sap.cap.esmapi.utilities.constants.GC_Constants;
+import com.sap.cloud.security.spring.config.IdentityServicesPropertySourceFactory;
+import com.sap.cloud.security.xsuaa.token.TokenAuthenticationConverter;
+
+@Configuration
+@EnableWebSecurity()
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
+@PropertySource(factory = IdentityServicesPropertySourceFactory.class, ignoreResourceNotFound = true, value = { "" })
+public class AppSecurityConfig {
+
+}
